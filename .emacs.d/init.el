ead2cb7114f6dc2bcb771820f5b3a05e8edf1d5a
ricardomga <ricardo.mga24@gmail.com>
2017-10-12 00:19:51 +0100

    emacs tweks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,34 +1,26 @@
+;;; package --- Summary
+;;; Commentary:
 ;; stuff automatically added
 
 
+;;; Code:
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default)))
- '(package-selected-packages
-   (quote
-    (powerline smart-mode-line auctex apropospriate-theme material material-theme ess php-mode projectile yasnippet web-mode base16-theme flycheck zenburn-theme color-theme auto-complete counsel ace-window org-bullets which-key try use-package)))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "simp" :slant normal :weight bold :height 113 :width normal))))
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-
-
-
-;;
+    ("70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "0961d780bd14561c505986166d167606239af3e2c3117265c9377e9b8204bf96" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))))
 
 ;; Our stuff
-
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -45,3 +37,7 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(provide 'init)
+;;; init.el ends here
+
